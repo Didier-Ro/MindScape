b5c431ba66e3ea4ee96c03ea1e72e8fd9c182344
Didier-Ro <ortizrojeroh@gmail.com>
2024-02-26 23:21:14 -0600

    Game Manager, Interactable Objects, UI Manager

---
+++ Assets/[Scripts]/GameManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singletone
+    private static GameManager Instance;
+    public static GameManager GetInstance() 
+    { 
+        return Instance;
+    }
+    #endregion
+
+    private GAME_STATE currentGameState = GAME_STATE.EXPLORIMG;
+    public Action<GAME_STATE> OnGameStateChange;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Application.targetFrameRate = 60;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ChangeGameState(GAME_STATE.EXPLORIMG);
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            ChangeGameState(GAME_STATE.PAUSE);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            ChangeGameState(GAME_STATE.READING);
+        }
+    }
+
+    public void ChangeGameState(GAME_STATE _newGameState)//When called, the current Game State changes to the new Game State and sends a notification to all subscribers that the Game State changed
+    {
+        currentGameState = _newGameState;
+
+        if (OnGameStateChange != null) 
+        {
+            OnGameStateChange.Invoke(currentGameState);
+        }
+    }
+
+    public GAME_STATE GetCurrentGameState()//When called, return the current Game State
+    {
+        return currentGameState;
+    }
+}
+
+public enum GAME_STATE //All possible Game States
+{
+    EXPLORIMG,
+    READING,
+    PAUSE,
+    CUTSCENES,
+    FLASBACKS,
+    DEAD
+}
---
+++ Assets/[Scripts]/InteractableObjects.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObjects : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            UIManager.GetInstance().InteractionUI(); 
+        }
+    }
+
+    public virtual void InteractionResponse()  
+    { 
+
+    }
+}
