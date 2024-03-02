570eec684d932660ee5cdd01430bf264007dfb0d
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2024-03-01 19:54:03 -0700

    si

--- Assets/[Scripts]/EnemySpawner.cs
+++ Assets/[Scripts]/EnemySpawner.cs
@@ -7,7 +7,7 @@ public class EnemySpawner : MonoBehaviour
     {
         GameObject enemy = PoolManager.GetInstance().GetPooledObject(OBJECT_TYPE.EnemyChase, new Vector2(1, 1), new Vector3(0, 0, 0));
         enemy.GetComponent<Enemy>().AssignTarget(target);
-        GameObject enemy2 =  PoolManager.GetInstance().GetPooledObject(OBJECT_TYPE.EnemyChase, new Vector2(2, 1), new Vector3(0, 0, 0));
+        GameObject enemy2 =  PoolManager.GetInstance().GetPooledObject(OBJECT_TYPE.EnemyChase, new Vector2(5, 1), new Vector3(0, 0, 0));
         enemy2.GetComponent<Enemy>().AssignTarget(target);
     }
 }
