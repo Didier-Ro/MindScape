026183b0de419930b304b9a1e4fbefa86d04bd53
Luis Francisco Pérez Hernández <lexius002@gmail.com>
2024-03-07 13:13:23 -0600

    IstepableFixed

--- Assets/[Scripts]/KeyInteraction.cs
+++ Assets/[Scripts]/KeyInteraction.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyInteraction : InteractableObjects
+public class KeyInteraction : MonoBehaviour, Istepable
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Activate()
     {
-        
+        Debug.Log("Reading");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Deactivate()
     {
-        
+        Debug.Log("Exit");
     }
 }
