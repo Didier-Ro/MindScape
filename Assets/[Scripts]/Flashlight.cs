f0c85108a01dd3686f1105a8a5eb5738f8435cf9
Miguel Alejandro Santiago Perez <miguelsantiagoews@gmail.com>
2024-03-07 23:03:24 -0600

    stamina and flashlight energy bar implemented

--- Assets/[Scripts]/Flashlight.cs
+++ Assets/[Scripts]/Flashlight.cs
@@ -1,26 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class Flashlight : MonoBehaviour
 {
+    public Slider slider;
     [SerializeField] private Light2D flashlight;
+    [SerializeField] private float energy = 100f; // Initial energy value
     private bool flashing = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        CircleLight();
+        InitializeFlashlight();
     }
 
     // Update is called once per frame
     void Update()
     {
+        HandleInput();
+        UpdateEnergyUI();
+    }
+
+    // Initialize the flashlight settings
+    private void InitializeFlashlight()
+    {
+        CircleLight();
+        energy = slider.maxValue;
+    }
+
+    // Handle input to toggle flashlight mode
+    private void HandleInput()
+    {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Flashing();
+            ToggleFlashing();
         }
 
         if (!flashing)
@@ -31,9 +45,19 @@ public class Flashlight : MonoBehaviour
         {
             ConcentrateLight();
         }
+
+        // Reduce energy based on flashlight mode
+        ReduceEnergy();
     }
 
-    private void CircleLight() 
+    // Update the energy UI slider
+    private void UpdateEnergyUI()
+    {
+        slider.value = energy;
+    }
+
+    // Set flashlight settings for circle light mode
+    private void CircleLight()
     {
         flashlight.intensity = 0.7f;
         flashlight.pointLightOuterRadius = 3;
@@ -42,17 +66,41 @@ public class Flashlight : MonoBehaviour
         flashlight.pointLightOuterAngle = 360;
     }
 
-    private void ConcentrateLight() 
+    // Set flashlight settings for concentrated light mode
+    private void ConcentrateLight()
     {
         flashlight.intensity = 1;
-        flashlight.pointLightOuterRadius = Mathf.Lerp(3,6, 1);
+        flashlight.pointLightOuterRadius = Mathf.Lerp(3, 6, 1);
         flashlight.pointLightInnerRadius = 0.24f;
         flashlight.pointLightInnerAngle = Mathf.Lerp(360, 26, 1);
         flashlight.pointLightOuterAngle = Mathf.Lerp(360, 26, 1);
     }
 
-    private void Flashing()
+    // Toggle between flashlight modes
+    private void ToggleFlashing()
     {
         flashing = !flashing;
+    }
+
+    // Reduce energy based on flashlight mode
+    private void ReduceEnergy()
+    {
+        if (!flashing)
+        {
+            energy -= Time.deltaTime; // Reduce energy slowly
+        }
+        else
+        {
+            energy -= Time.deltaTime * 4f; // Reduce energy faster
+        }
+
+        // Clamp energy to ensure it stays within valid range
+        energy = Mathf.Clamp(energy, 0f, slider.maxValue);
+    }
+
+    // Function to get current energy level
+    public float GetEnergy()
+    {
+        return energy;
     }
 }
