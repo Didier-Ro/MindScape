21b0ec975eb61faf80a65590f75acf3b85e68e8f
Luis Francisco Pérez Hernández <lexius002@gmail.com>
2024-03-08 15:41:57 -0600

    PlayerPrefab

--- Assets/[Scripts]/PlayerMovement.cs
+++ Assets/[Scripts]/PlayerMovement.cs
@@ -6,17 +6,35 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private Rigidbody2D _rb;
+    private Vector2 _moveInputValue = Vector2.zero;
     public Animator animator;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
-    void Update()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        _rb.velocity = new Vector2(horizontal * _speed, vertical * _speed);
-        animator.SetFloat("x", horizontal);
+       
+        MovePlayer();
+    }
+    
+    void MovePlayer()
+    {
+        _moveInputValue = InputManager.GetInstance().MovementInput();
+       
+        Vector2 movement = new Vector2(_moveInputValue.x, _moveInputValue.y);
+        animator.SetFloat("x", _moveInputValue.x);
+
+        movement.Normalize(); // Evitar movimientos diagonales más rápidos
+
+        _rb.velocity = movement * _speed;
+
+        // Rotar el jugador hacia la dirección del movimiento
+        if (movement != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 
     
