7e8fedcc734ff349371a4d5124c79580c860feb4
Luis Francisco Pérez Hernández <lexius002@gmail.com>
2024-03-15 14:46:18 -0600

    uwu

--- Assets/[Scripts]/BoxTp.cs
+++ Assets/[Scripts]/BoxTp.cs
@@ -7,14 +7,39 @@ public class BoxTp : MonoBehaviour
 {
     [SerializeField] private Transform teleportA = default;
     [SerializeField] private Transform teleportB = default;
+    [SerializeField] private GameObject compatibleBox = default;
+    [SerializeField] private bool isInteracting = false;
+    [SerializeField] private bool canInteract;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void FixedUpdate()
     {
-        if (other.gameObject.CompareTag("Box"))
+        TeleportBox();
+    }
+
+    public void TeleportBox()
+    {
+        if (canInteract && InputManager.GetInstance().InteractInput())
         {
+            compatibleBox.transform.position = teleportB.position;
             
-            other.transform.position = teleportB.position;
         }
-        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D _collision)
+    {
+        if (_collision.tag == ("Player") && IsInTp.GetInstance().SetBoxState())
+        {
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D _collision)
+    {
+        if (_collision.tag == ("Player"))
+        {
+           
+            canInteract = false;
+            isInteracting = false;
+        }
     }
 }
