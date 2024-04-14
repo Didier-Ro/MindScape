a359d01a5b7d264ffb29e98650e396c18041fdab
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2024-04-13 18:39:00 -0700

    conexión entre la animación y el menu principal

--- Assets/[Scripts]/DataManager/ChangeGame.cs
+++ Assets/[Scripts]/DataManager/ChangeGame.cs
@@ -4,8 +4,46 @@ using UnityEngine;
 
 public class ChangeGame : MonoBehaviour
 {
+   [SerializeField] private WorldCondition stageConditions;
+   [SerializeField] private WorldCondition[] allConditions;
+   [SerializeField] private string gameScene;
+   [SerializeField] private string animationScene;
+   
+   
+   private void ResetAll()
+   {
+      for (int i = 0; i < allConditions.Length; i++)
+      {
+         allConditions[i].ResetData();
+      }
+   }
+   
+   private void LoadAllData()
+   {
+      string[] dataToLoad = PlayerPrefs.GetString("alldata").Split("*");
+      for (int i = 0; i < allConditions.Length; i++)
+      {
+         allConditions[i].LoadData(dataToLoad[i]);
+      }
+   }
+   
+   private void LoadCurrentGameData(int _currentGame)
+   {
+      foreach (var stageCondition in allConditions)
+      {
+         if (stageCondition.nGame == _currentGame)
+         {
+            stageConditions = stageCondition.GetNumOfGame();
+         }
+      }
+   }
    public void SelectGame(int _num)
    {
+      ResetAll();
+      LoadAllData();
       PlayerPrefs.SetInt("GameNumber", _num);
+      LoadCurrentGameData(_num);
+      string sceneToPlay = stageConditions.IsFirstTimePlayed() ? animationScene : gameScene; 
+      LoadingManager.instance.LoadScene(sceneToPlay);
    }
 }
