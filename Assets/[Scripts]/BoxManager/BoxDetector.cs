6043cc964a50a4d6184138bd6bf235f02e3b4b96
Didier-Ro <ortizrojeroh@gmail.com>
2024-04-18 19:17:29 -0600

    BoxDetector

--- Assets/[Scripts]/BoxManager/BoxDetector.cs
+++ Assets/[Scripts]/BoxManager/BoxDetector.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class BoxDetector : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D colliderParent;
     [SerializeField] private Vector2 spawnPos;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Box"))
         {
-            BoxCollider2D collider = collision.GetComponent<BoxCollider2D>();
-            BoxCollider2D childrenCollider = collision.GetComponentInChildren<BoxCollider2D>();
-            collider.enabled = false;
-            childrenCollider.enabled = false;
+            Transform parentTransform = collision.transform.parent;
+            GameObject parent = parentTransform.gameObject;        
+            colliderParent = parent.GetComponent<BoxCollider2D>();
+            colliderParent.enabled = false;
             PoolManager.GetInstance().GetPooledObject(OBJECT_TYPE.Box, spawnPos, new Vector3(0, 0, 0));
         }
     }
