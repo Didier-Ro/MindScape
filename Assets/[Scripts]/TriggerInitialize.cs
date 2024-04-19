8e9b17a52f51bac0b6bbcbc9eca8c7d35d32c419
Pollogamerz <tbarajasglez05@gmail.com>
2024-04-19 01:07:42 -0600

    cambios

--- Assets/[Scripts]/TriggerInitialize.cs
+++ Assets/[Scripts]/TriggerInitialize.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class TriggerInitialize : MonoBehaviour
 {
+    public DoorScript doorScript;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-           EnemySpawner.getInstance().SpawnRound(5, OBJECT_TYPE.EnemyChase);
+            doorScript.isUnlocked = false;
+            EnemySpawner.getInstance().SpawnRound(5, OBJECT_TYPE.EnemyChase);
            Destroy(gameObject);
         }
     }
