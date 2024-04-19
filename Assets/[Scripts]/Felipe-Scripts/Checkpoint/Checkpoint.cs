b9df19cd0245169ffb3f0e2399263b39066c1cc7
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2024-04-18 23:14:05 -0700

    a

--- Assets/[Scripts]/Felipe-Scripts/Checkpoint/Checkpoint.cs
+++ Assets/[Scripts]/Felipe-Scripts/Checkpoint/Checkpoint.cs
@@ -1,16 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
     public Vector3 checkpointPosition;
+    [SerializeField] private int condition;
+    [SerializeField] private bool isSavedInGame = false;
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             CheckpointManager.AddCheckpointPosition(checkpointPosition);
+            if (isSavedInGame)
+            {
+                GameManager.GetInstance().SavePlayerPosition(checkpointPosition);
+                GameManager.GetInstance().MarkConditionCompleted(condition);
+            }
         }
     }
 }
