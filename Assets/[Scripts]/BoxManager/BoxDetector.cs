0781b9203db98b7bf5bab4520581a6462a33e47c
Luis Francisco Pérez Hernández <lexius002@gmail.com>
2024-04-19 00:21:42 -0600

    Cajas

--- Assets/[Scripts]/BoxManager/BoxDetector.cs
+++ Assets/[Scripts]/BoxManager/BoxDetector.cs
@@ -7,15 +7,54 @@ public class BoxDetector : MonoBehaviour
     [SerializeField] private GameObject boxPrefab;
     [SerializeField] private BoxCollider2D colliderParent;
     [SerializeField] private Vector2 spawnPos;
+    [SerializeField] private TYPE_DETECTOR typeDetector;
+    [SerializeField] private Doors doors;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Box"))
         {
-            Transform parentTransform = collision.transform.parent;
-            GameObject parent = parentTransform.gameObject;        
-            colliderParent = parent.GetComponent<BoxCollider2D>();
-            colliderParent.enabled = false;
-            GameObject obj = Instantiate(boxPrefab, spawnPos, Quaternion.identity);
+            if (typeDetector == TYPE_DETECTOR.HOLE)
+            {
+                Transform parentTransform = collision.transform.parent;
+                GameObject parent = parentTransform.gameObject;        
+                colliderParent = parent.GetComponent<BoxCollider2D>();
+                colliderParent.enabled = false;
+                GameObject obj = Instantiate(boxPrefab, spawnPos, Quaternion.identity);
+                gameObject.SetActive(false);
+            }
+            else if (typeDetector == TYPE_DETECTOR.BUTTON)
+            { 
+                doors.IncreaseCounter();
+                if (doors.ReturnCounter() != 2)
+                {
+                    Transform parentTransform = collision.transform.parent;
+                    GameObject parent = parentTransform.gameObject;        
+                    colliderParent = parent.GetComponent<BoxCollider2D>();
+                    colliderParent.enabled = false;
+                    GameObject obj = Instantiate(boxPrefab, spawnPos, Quaternion.identity);
+                    gameObject.SetActive(false);
+                }
+            }
+            else if (typeDetector == TYPE_DETECTOR.UNIQUE)
+            {
+                doors.IncreaseCounter();
+              
+                    Transform parentTransform = collision.transform.parent;
+                    GameObject parent = parentTransform.gameObject;        
+                    colliderParent = parent.GetComponent<BoxCollider2D>();
+                    colliderParent.enabled = false;
+                    gameObject.SetActive(false);
+            }
+            
         }
     }
 }
+
+public enum TYPE_DETECTOR
+{
+    HOLE,
+    BUTTON,
+    UNIQUE
+}
+
+
