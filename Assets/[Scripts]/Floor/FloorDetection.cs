d020cc2b408ec6a252fe8a7abd4b5115fce2d5cd
Justpeli <83651771+Justpeli@users.noreply.github.com>
2024-05-02 09:49:23 -0600

    Revert "Merge branch 'main' into Peli2.0"
    This reverts commit 768e36e22d364692545e0491a977c242473448c8, reversing
    changes made to d9332353884d2aa8dd257bf2251f598c678aa91c.

--- Assets/[Scripts]/Floor/FloorDetection.cs
+++ Assets/[Scripts]/Floor/FloorDetection.cs
@@ -4,34 +4,18 @@ using UnityEngine;
 
 public class FloorDetection : MonoBehaviour
 {
-    private List<BoxCollider2D> boxColliders = new List<BoxCollider2D>(); 
-    private GameObject floor;
+    [SerializeField] private GameObject floor;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == ("Floor"))
         {
-            boxColliders.Add(collision.GetComponent<BoxCollider2D>());
-            floor = boxColliders[boxColliders.Count - 1].gameObject;
-
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == ("Floor"))
-        {
-            if(boxColliders.Count > 2)
-            {
-                boxColliders.RemoveAt(0);
-                floor = boxColliders[boxColliders.Count - 1].gameObject;
-            }
+            floor = collision.gameObject; 
         }
     }
 
     public void ActivateFloorSound()
     {
-        
         floor.GetComponent<Fstepable>().FActivate();
     }
 }
