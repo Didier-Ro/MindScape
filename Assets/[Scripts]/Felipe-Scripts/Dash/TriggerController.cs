f72e0ea27bdc6cfeed15a9f8e98d6d3345979677
Pollogamerz <tbarajasglez05@gmail.com>
2024-05-03 14:55:08 -0600

    Tutorial del dash
    -Se agregaron unos objetos vacios hechos prefabs para hacer el tutorial del dash, es lo unico de la escena que puse
    -Se hizo un nuevo codigo llamado TriggerController y se modificó el dashcontroller para que pueda usarse una vez el tutorial y de vuelta ya no
    -Y ya

---
+++ Assets/[Scripts]/Felipe-Scripts/Dash/TriggerController.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour
+{
+    public GameObject dashText;
+    public GameObject jumpText;
+
+    private bool isActive = true;
+    private bool activated = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && isActive && !activated)
+        {
+            activated = true;
+            if (gameObject.name == "DashTrigger")
+            {
+                dashText.SetActive(true);
+            }
+            else if (gameObject.name == "JumpTrigger")
+            {
+                jumpText.SetActive(true);
+            }
+            FindObjectOfType<DashController>().SetCurrentTrigger(this);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dashText.SetActive(false);
+            jumpText.SetActive(false);
+            activated = false;
+        }
+    }
+    public void DisableTrigger()
+    {
+        isActive = false;
+    }
+}
