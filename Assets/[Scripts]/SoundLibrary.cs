ead9109fc5b0a27ddde28f9f9a191814570c713e
Justpeli <83651771+Justpeli@users.noreply.github.com>
2024-05-02 20:38:23 -0600

    AudioManager
    -Added new AudioManager
    -Remove the old one

---
+++ Assets/[Scripts]/SoundLibrary.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundLibrary", menuName = "Scriptables/SoundLibrary", order = 1)]
+public class SoundLibrary : ScriptableObject
+{
+    [SerializeField] SoundArchive[] soundArchives;
+
+    public AudioClip GetRandomSoundFromType(SOUND_TYPE _st)
+    {
+        for (int i = 0; i < soundArchives.Length; i++)
+        {
+            if (soundArchives[i].soundType == _st)
+            {
+                int randomnum = UnityEngine.Random.Range(0, soundArchives[i].sounds.Length);
+                return soundArchives[i].sounds[randomnum];
+            }
+        }
+        return null;
+    }
+}
+[Serializable]
+public class SoundArchive
+{
+    public SOUND_TYPE soundType;
+    public AudioClip[] sounds;
+}
+public enum SOUND_TYPE
+{
+    PASOS_CONCRETO,
+    PASOS_MADERA,
+    FUEGO = 3,
+    GOLPE,
+    MUERTE_ENEMIGO,
+    PASOS_ALFOMBRA = 2,
+    GOLPE_ENEMIGO,
+
+}
