4c6886c7c4a57f93b3efaf6073e5eb978d4b8673
Pollogamerz <tbarajasglez05@gmail.com>
2024-05-10 16:57:12 -0600

    iniciodeanimacionllave
    codigo modificado para inicio de animacion de la llave

--- Assets/[Scripts]/Felipe-Scripts/DoorScripts/DoorScript.cs
+++ Assets/[Scripts]/Felipe-Scripts/DoorScripts/DoorScript.cs
@@ -14,7 +14,7 @@ public class DoorScript : MonoBehaviour
     float time;
     private void Awake()
     {
-        Instance = this; // Establecer la instancia estática al iniciar
+        Instance = this;
     }
     void Start()
     {
