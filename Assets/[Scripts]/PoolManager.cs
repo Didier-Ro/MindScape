5f878e3f22c7de2a72cae41b96afa039346077b9
Pollogamerz <tbarajasglez05@gmail.com>
2024-05-13 18:07:58 -0600

    mejoras y sistema pool particulas
    Eh puesto el sistema de particulas pool por ahora le puse a los pies, y acomode los enum y prefabs de particulas

--- Assets/[Scripts]/PoolManager.cs
+++ Assets/[Scripts]/PoolManager.cs
@@ -48,16 +48,17 @@ public class PoolManager : MonoBehaviour
     {
         isPaused = _newGameState == GAME_STATE.EXPLORATION;
     }
-    
+
     #endregion
-    
-    public GameObject GetPooledObject(OBJECT_TYPE _type, Vector2 coordinateToSpawn, Vector3 rotation) //Create or Activate the GameObject you want
+
+    public GameObject GetPooledObject(OBJECT_TYPE _type, Vector2 coordinateToSpawn, Vector3 rotation)
     {
         for (int i = 0; i < _pooledObjects.Count; i++)
         {
             if (!_pooledObjects[i].activeInHierarchy)
             {
-                if (_pooledObjects[i].GetComponent<ObjectType>().GetObjectType() == _type )
+                ObjectType objectType = _pooledObjects[i].GetComponent<ObjectType>();
+                if (objectType != null && objectType.GetObjectType() == _type)
                 {
                     _pooledObjects[i].transform.position = coordinateToSpawn;
                     _pooledObjects[i].SetActive(true);
@@ -68,17 +69,18 @@ public class PoolManager : MonoBehaviour
 
         for (int i = 0; i < objectsToSpawn.Length; i++)
         {
-            if (objectsToSpawn[i].GetComponent<ObjectType>().GetObjectType() == _type)
+            ObjectType objectType = objectsToSpawn[i].GetComponent<ObjectType>();
+            if (objectType != null && objectType.GetObjectType() == _type)
             {
-                GameObject currentBullet = Instantiate(objectsToSpawn[i],
-                    coordinateToSpawn, Quaternion.Euler(rotation));
-                _pooledObjects.Add(currentBullet);
-                return currentBullet;
+                GameObject currentObject = Instantiate(objectsToSpawn[i], coordinateToSpawn, Quaternion.Euler(rotation));
+                _pooledObjects.Add(currentObject);
+                return currentObject;
             }
         }
+
         return null;
     }
-    
+
 }
 
 public enum OBJECT_TYPE
