8d2f339ac0dcf9030b8a89c7081a5fd4e52605bb
Didier-Ro <ortizrojeroh@gmail.com>
2024-05-16 21:12:23 -0600

    shadow bug
    Las sombras aparecen aunque no esté la linterna activa

--- Assets/[Scripts]/Flashlight.cs
+++ Assets/[Scripts]/Flashlight.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
@@ -50,6 +51,9 @@ public class Flashlight : MonoBehaviour
     private float offsetAngle = 270;
     private float lastAngle = 0;
 
+    public Action<LIGHT_ENERGY_STATE> OnLightEnergyChange;
+    public LIGHT_ENERGY_STATE lightEnergyState = LIGHT_ENERGY_STATE.ON;
+
     private void Awake()
     {
         if (Instance == null)
@@ -71,12 +75,51 @@ public class Flashlight : MonoBehaviour
         angleRange = minPointLightOuterAngle / 2;
     }
 
+    private void Update()
+    {
+        if (flashlight.isActiveAndEnabled)
+        {
+            ChangeEnergyState(LIGHT_ENERGY_STATE.ON);
+        }
+        else
+        {
+            ChangeEnergyState(LIGHT_ENERGY_STATE.OFF);
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         if (!isExPloration)
             return;
         HandleInput();
+    }
+
+    private void OnEnable()
+    {
+        if (flashlight.isActiveAndEnabled)
+        {
+            ChangeEnergyState(LIGHT_ENERGY_STATE.ON);
+        }
+        else
+        {
+            ChangeEnergyState(LIGHT_ENERGY_STATE.OFF);
+        }
+    }
+
+    public void ChangeEnergyState(LIGHT_ENERGY_STATE _energyState)
+    {
+        lightEnergyState = _energyState;
+
+        if (OnLightEnergyChange != null)
+        {
+            OnLightEnergyChange.Invoke(lightEnergyState);
+        }
+    }
+
+    public LIGHT_ENERGY_STATE GetLightEnergyState()
+    {
+        return lightEnergyState;
     }
 
     private void SubscribeToGameManagerGameState()//Subscribe to Game Manager to receive Game State notifications when it changes
@@ -312,4 +355,9 @@ public class Flashlight : MonoBehaviour
     {
         return energy;
     }
+}
+
+public enum LIGHT_ENERGY_STATE{
+    ON,
+    OFF
 }
