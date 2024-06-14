0d26694c7c524cb445c7422cfb53c55b62f8b1bf
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2024-06-14 11:18:32 -0700

    camios ahora

--- Assets/[Scripts]/MistZone/MistZone.cs
+++ Assets/[Scripts]/MistZone/MistZone.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class MistZone : MonoBehaviour
 {
     [SerializeField] private float damagePerSecond;
-    [SerializeField]private GameObject playerRef;
+    [SerializeField] private GameObject playerRef;
     private Flashlight flashLight;
     private HealthController healthController;
     private bool playerInZone;
     private bool canDamage;
+    
+    [SerializeField] private SoundLibrary soundLibrary;
+    private AudioSource mistAudioSource;
+    
+    private AudioSource musicAudioSource;
+    [SerializeField] private float musicVolumeInMist = 0.2f;
+    private float originalMusicVolume;
 
     private void Start()
     {
@@ -18,6 +25,19 @@ public class MistZone : MonoBehaviour
         {
             flashLight = playerRef.GetComponent<Flashlight>();
             healthController = playerRef.GetComponent<HealthController>();
+        }
+        
+        mistAudioSource = GetComponent<AudioSource>();
+        if (mistAudioSource == null)
+        {
+            mistAudioSource = gameObject.AddComponent<AudioSource>();
+        }
+        
+        GameObject musicObject = GameObject.Find("Musica");
+        if (musicObject != null)
+        {
+            musicAudioSource = musicObject.GetComponent<AudioSource>();
+            originalMusicVolume = musicAudioSource.volume;
         }
     }
 
@@ -29,7 +49,12 @@ public class MistZone : MonoBehaviour
         }
         else
         {
-            canDamage = false;  
+            canDamage = false;
+        }
+
+        if (playerInZone && !mistAudioSource.isPlaying)
+        {
+            PlayMistSound();
         }
     }
 
@@ -43,10 +68,11 @@ public class MistZone : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
         if (collision.gameObject.CompareTag("Player"))
         {
             playerInZone = true;
+            PlayMistSound();
+            ReduceMusicVolume();
         }
     }
 
@@ -55,6 +81,8 @@ public class MistZone : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             playerInZone = false;
+            StopMistSound();
+            RestoreMusicVolume();
         }
     }
 
@@ -62,5 +90,37 @@ public class MistZone : MonoBehaviour
     {
         float totalDamage = damagePerSecond / 60;
         healthController.PlayerTakeDamage(totalDamage);
+    }
+
+    private void PlayMistSound()
+    {
+        AudioClip mistSound = soundLibrary.GetRandomSoundFromType(SOUND_TYPE.Neblina);
+        if (mistSound != null)
+        {
+            mistAudioSource.clip = mistSound;
+            mistAudioSource.loop = true;
+            mistAudioSource.Play();
+        }
+    }
+
+    private void StopMistSound()
+    {
+        mistAudioSource.Stop();
+    }
+
+    private void ReduceMusicVolume()
+    {
+        if (musicAudioSource != null)
+        {
+            musicAudioSource.volume = musicVolumeInMist;
+        }
+    }
+
+    private void RestoreMusicVolume()
+    {
+        if (musicAudioSource != null)
+        {
+            musicAudioSource.volume = originalMusicVolume;
+        }
     }
 }
